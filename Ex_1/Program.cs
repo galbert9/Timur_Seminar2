e5eae8346832016a6f13988d0178a9f24fb56d1f
Gennady <galbert9@gmail.com>
2022-11-09 18:55:29 +0300

    Сгенерировать случайное число и найти в нем Макс цифру

---
+++ Ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Сгенерировать случайное число и найти в нем Макс цифру
+int x = new Random().Next(10,100);
+
+int x1 = x/10;
+
+int x2 = x % 10;
+
+Console.WriteLine($"Случайное число {x}");
+Console.WriteLine($"Первая цифра {x1}");
+Console.WriteLine($"Вторая цифра {x2}");
+
+if (x1 > x2) Console.WriteLine($" Максимум это {x1}");
+else if (x1 < x2) Console.WriteLine($"Максимум равен {x2}");
+else Console.WriteLine($"Обе цифры равны");
