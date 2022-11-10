bb2e62a17ba7a8560de44a75e4e8544d51c2df6b
Gennady <galbert9@gmail.com>
2022-11-10 11:29:18 +0300

    Проверка на кратность одновременно двум заданным числам

---
+++ Ex_4/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задать число и проверить его кратность одновременно 7 и 23
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32 (Console.ReadLine());
+int c = a % 7;
+int d = a % 23;
+if (c == 0 & d == 0)
+Console.WriteLine("Кратно");
+ else Console.WriteLine("Не кратно");
