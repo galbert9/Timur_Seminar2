eeba243fd2eeca5d1bb71aaca7019b6985f6d0ec
Gennady <galbert9@gmail.com>
2022-11-10 11:02:10 +0300

    Проверка на кратность

---
+++ Ex_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// На входе два числа. Проверить кратность, если нет,
+// вывести остаток от деления 19:22
+
+Console.WriteLine("Введите первое число");  
+int a = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Введите второе число"); 
+int b = Convert.ToInt32 (Console.ReadLine());
+int c = a % b;
+
+if (c == 0) Console.WriteLine("Числа кратные");
+else Console.WriteLine("Числа не кратные, остаток = "+c);
